339f6c01ff4e1c821086e54fdaf75351298210ea
rayGreycloud <rayGreycloud@gmail.com>
2018-01-28 12:37:10 -0800

    Clean out boilerplate text and logo

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import web3 from './web3.js';
 
 class App extends Component {
   render() {
+    web3.eth.getAccounts()
+      .then(console.log);
+    
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          
+          <h1 className="App-title">Welcome to the Lottery</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To get started, you must have the Metamask Chrome extension installed.
         </p>
       </div>
     );
