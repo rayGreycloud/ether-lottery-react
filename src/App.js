11ff42f949eb6aa7a9a350323fbb550bf35e5d1b
rayGreycloud <rayGreycloud@gmail.com>
2018-01-28 20:06:18 -0800

    Get and display players and balance

--- src/App.js
+++ src/App.js
@@ -5,20 +5,35 @@ import lottery from './lottery';
 
 class App extends Component {
   state = {
-    manager: ''
+    manager: '',
+    players: [],
+    balance: ''
   };
   
   async componentDidMount() {
     const manager = await lottery.methods.manager().call();
+    const players = await lottery.methods.getPlayers().call();
+    const balance = await web3.eth.getBalance(lottery.options.address);
     
-    this.setState({ manager });
+    this.setState({ manager, players, balance });
   }
   
   render() {  
     return (
-      <div className="App App-header">
-        <h2 className="App-title">Lottery Contract</h2>
-        <p className="App-intro">This contract is managed by: </p><p>{this.state.manager}</p>
+      <div className="App">
+        <div className="App-header">
+          <h2 className="App-title">Lottery Contract</h2>  
+          
+        </div>
+        <div className="content">
+          <p>
+            This contract is managed by:<br/> {this.state.manager}
+          </p>
+          <p>
+            There are currently <strong> {this.state.players.length}</strong> player(s) 
+          </p>
+          <p>Prize Pool: {web3.utils.fromWei(this.state.balance, 'ether')} ether</p>           
+        </div>      
       </div>
     );
   }
